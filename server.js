b3d6e5a211fec35531161f5fb5144381deff83fd
irakevine <ikevine2016@gmail.com>
2023-08-09 00:57:44 +0200

    creating connection

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const sequelize = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect:  'postgres' 
 });
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
