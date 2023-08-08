afcc97c3efdbd11105978ab725e6e7c7f8fcd426
irakevine <ikevine2016@gmail.com>
2023-08-09 01:13:23 +0200

    creating connection

--- src/server.js
+++ src/server.js
@@ -1,4 +1,15 @@
+import express from 'express'
 
+const app = express()
+const   PORT = 3000 || Process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`)
+})
 
 
 
