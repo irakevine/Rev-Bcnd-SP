cd53ba8d103f80acde709cd3dea96489e62f9b80
irakevine <ikevine2016@gmail.com>
2023-08-09 01:04:14 +0200

    creating connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-// Connection
+// Connection 
 const sequelize = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect:  'postgres' 
