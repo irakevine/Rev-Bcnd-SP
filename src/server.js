67bc29f52328247db22f115ed912e8fe88d5615a
irakevine <ikevine2016@gmail.com>
2023-08-09 01:24:46 +0200

    creating connection

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import path from 'path';
+import dotenv from 'dotenv';
+
+
+// CONFIGURE DOTENV
+dotenv.config();
+
 const app = express()
-const   PORT = 3000 || Process.env.PORT
+const { PORT } = process.env;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+// CREATE EXPRESS SERVER
+const server = app.listen(PORT);
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
