799cdb9338730e682f8403428ab4c09676f1af36
irakevine <ikevine2016@gmail.com>
2023-08-09 20:27:46 +0200

    adding connection db

---
+++ database/config/config.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const { DB_USER, DB_PASSWORD, DB_NAME, DB_PORT,DB_URL } = process.env;
+module.exports = {
+  development: {
+      use_env_variable:"DB_URL",
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      database_url: process.env.DB_URL,
+      port: process.env.DB_PORT,
+      dialectOptions: {},
+      dialect: "postgres",
+      logging:false
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    database_url: DB_URL,
+    port: DB_PORT,
+    host: DB_HOST,
+    dialect: "postgres"
+}
+  
+}
--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
-import express from 'express';
+import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
+import express from 'express';
+import db from '../database/models/index.js';
 
-// CONFIGURE DOTENV
-dotenv.config();
 
+dotenv.config()
 const app = express();
-const { PORT } = process.env;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+console.log('hell');
+const {DB_NAME,DB_USER,DB_PASSWORD,DB_PORT,PORT}=process.env
+const server = app.listen(PORT);
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres'
 });
+const dbCon = async () => {
+  try {
+    await db.sequelize.authenticate();
+  } catch (error) {
+    console.log(`db error: ${error.message}`);
+  }
+};
 
-// CREATE EXPRESS SERVER
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+Promise.all([dbCon(), server]).then(() => {
+  console.log(`Server listening on port:${PORT}`);
+  console.log('DB connection successful');
 });
