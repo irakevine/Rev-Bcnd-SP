0fe776dbc2d07d2f41ce36da19123eb8d8e7038f
irakevine <ikevine2016@gmail.com>
2023-08-11 12:05:22 +0200

    modifying

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
+import { Sequelize } from 'sequelize'
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
-import db from '../database/models/index.js';
+import dotenv from 'dotenv';
+// import {  db } from '../database/models/index.js'
 
 
 const app = express();
 dotenv.config()
 app.use(cors({ origin: '*' }));
-app.set('view engine', 'ejs');
+
 console.log('hell');
 const {DB_NAME,DB_USER,DB_PASSWORD,DB_PORT,PORT}=process.env
 const server = app.listen(PORT);
