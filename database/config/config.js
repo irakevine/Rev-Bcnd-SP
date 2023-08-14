2077f45e987db4ded0246ca1c81acb9fecbc2479
irakevine <ikevine2016@gmail.com>
2023-08-14 21:31:35 +0200

    modfying

--- database/config/config.js
+++ database/config/config.js
@@ -4,23 +4,22 @@ dotenv.config();
 
 const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DB_URL } = process.env;
 module.exports = {
-    development: {
-        username: DB_USER,
-        password: DB_PASSWORD,
-        database: DB_NAME,
-        database_url: DB_URL,
-        host: DB_HOST,
-        port: DB_PORT,
-        dialect: "postgres"
-    },
-    production: {
-        username: DB_USER,
-        password: DB_PASSWORD,
-        database: DB_NAME,
-        database_url: DB_URL,
-        port: DB_PORT,
-        host: DB_HOST,
-        dialect: "postgres"
-    }
-}
-
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    database_url: DB_URL,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    database_url: DB_URL,
+    port: DB_PORT,
+    host: DB_HOST,
+    dialect: "postgres",
+  },
+};
