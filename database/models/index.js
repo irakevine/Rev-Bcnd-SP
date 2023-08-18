cd6ab4a50996f49dfd47b6cae9af831d86ae917c
kevine12 <ikevine2016@gmail.com>
2023-08-18 13:53:18 +0200

    removing unseccary data in indexjs

--- database/models/index.js
+++ database/models/index.js
@@ -9,17 +9,10 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const sequelize = new Sequelize(config.database_url || process.env.DB_URL, {
+  dialect: config.dialect,
+  host: config.host,
+});
 
 fs.readdirSync(__dirname)
   .filter((file) => {
